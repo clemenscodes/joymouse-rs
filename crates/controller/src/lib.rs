51b4e0d1bb096767d44f6f9d5f7d9d851ffbc399
Clemens Horn <clemens.horn@mni.thm.de>
2025-07-13 22:09:55 +0200

    abstract event emission

--- crates/controller/src/lib.rs
+++ crates/controller/src/lib.rs
@@ -1,15 +1,58 @@
 mod button;
+mod error;
 mod joystick;
 
 use std::sync::{Arc, Mutex};
 
 pub use button::*;
+pub use error::*;
 pub use joystick::*;
 
-pub trait ControllerEventEmitter: Send + Sync {}
+pub enum ControllerEvent {
+  Button(ButtonEvent),
+  JoyStick(JoyStickEvent),
+}
+
+pub trait ControllerEventEmitter: Send + Sync {
+  fn emit(&mut self, events: &[ControllerEvent]) -> Result<(), ControllerError>;
+}
 
 pub struct VirtualController {
   emitter: Box<dyn ControllerEventEmitter>,
   left_stick: Arc<Mutex<JoyStickState>>,
   right_stick: Arc<Mutex<JoyStickState>>,
+}
+
+impl VirtualController {
+  pub fn new(
+    emitter: Box<dyn ControllerEventEmitter>,
+    left_stick: Arc<Mutex<JoyStickState>>,
+    right_stick: Arc<Mutex<JoyStickState>>,
+  ) -> Self {
+    Self {
+      emitter,
+      left_stick,
+      right_stick,
+    }
+  }
+
+  pub fn emit(&mut self, events: &[ControllerEvent]) -> Result<(), ControllerError> {
+    self.emitter.emit(events)
+  }
+
+  pub fn left_stick(&self) -> &Mutex<JoyStickState> {
+    &self.left_stick
+  }
+
+  pub fn right_stick(&self) -> &Mutex<JoyStickState> {
+    &self.right_stick
+  }
+
+  pub fn left_stick_mut(&mut self) -> &mut Arc<Mutex<JoyStickState>> {
+    &mut self.left_stick
+  }
+
+  pub fn right_stick_mut(&mut self) -> &mut Arc<Mutex<JoyStickState>> {
+    &mut self.right_stick
+  }
 }
