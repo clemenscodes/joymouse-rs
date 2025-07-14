1cabd9ed26d5fbd9f6993b5391ebc185a5b3b477
Clemens Horn <clemens.horn@mni.thm.de>
2025-07-14 17:59:14 +0200

    move joystick and state out of platform

--- crates/platform/src/joystick/mod.rs
+++ crates/platform/src/joystick/mod.rs
@@ -3,58 +3,21 @@ mod control;
 mod event;
 mod keys;
 mod polarity;
-mod state;
 
-use controller::JoyStickError;
+use controller::{JoyStick, JoyStickError};
 
-use evdev::{KeyCode, KeyEvent, RelativeAxisCode, RelativeAxisEvent};
+use evdev::RelativeAxisCode;
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum JoyStick {
-  Left,
-  Right,
-}
-
-impl TryFrom<RelativeAxisCode> for JoyStick {
-  type Error = JoyStickError;
-
-  fn try_from(value: RelativeAxisCode) -> Result<Self, Self::Error> {
-    let joystick = match value {
-      RelativeAxisCode::REL_X => Self::Right,
-      RelativeAxisCode::REL_Y => Self::Right,
-      other => return Err(JoyStickError::UnsupportedCode(other.0)),
-    };
-    Ok(joystick)
-  }
-}
-
-impl TryFrom<RelativeAxisEvent> for JoyStick {
-  type Error = JoyStickError;
-
-  fn try_from(value: RelativeAxisEvent) -> Result<Self, Self::Error> {
-    Self::try_from(value.code())
-  }
-}
-
-impl TryFrom<KeyCode> for JoyStick {
-  type Error = JoyStickError;
-
-  fn try_from(value: KeyCode) -> Result<Self, Self::Error> {
-    if !JOYSTICK_KEYS.code_is_joystick_key(value) {
-      return Err(JoyStickError::UnsupportedCode(value.0));
-    }
-    Ok(JoyStick::Left)
-  }
-}
-
-impl TryFrom<KeyEvent> for JoyStick {
-  type Error = JoyStickError;
-
-  fn try_from(value: KeyEvent) -> Result<Self, Self::Error> {
-    Self::try_from(value.code())
-  }
+pub fn try_from_relative_axis_code_for_joystick(
+  code: RelativeAxisCode,
+) -> Result<JoyStick, JoyStickError> {
+  let joystick = match code {
+    RelativeAxisCode::REL_X => JoyStick::Right,
+    RelativeAxisCode::REL_Y => JoyStick::Right,
+    other => return Err(JoyStickError::UnsupportedCode(other.0)),
+  };
+  Ok(joystick)
 }
 
 pub use event::ControllerJoyStickEvent;
 pub use keys::JOYSTICK_KEYS;
-pub use state::JoyStickState;
