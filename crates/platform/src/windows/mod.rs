9007fb09ac5067649dc088f304194383255ee68a
Clemens Horn <clemens.horn@mni.thm.de>
2025-07-15 09:02:52 +0200

    prepare windows support

--- crates/platform/src/windows/mod.rs
+++ crates/platform/src/windows/mod.rs
@@ -1,7 +1,8 @@
 use std::sync::{Arc, Mutex};
 
 use controller::{
-  ControllerError, ControllerEvent, ControllerEventEmitter, JoyStickState, VirtualController,
+  ControllerError, ControllerEvent, ControllerEventEmitter, JoyStickState,
+  PlatformControllerManager, PlatformControllerOps, VirtualController, VirtualControllerCore,
 };
 
 pub struct Controller {
@@ -11,8 +12,7 @@ pub struct Controller {
 
 impl ControllerEventEmitter for Controller {
   fn emit(&mut self, events: &[ControllerEvent]) -> Result<(), ControllerError> {
-    println!("Emitting events: {:#?}", events);
-    Ok(())
+    todo!("[Windows] Emit controller events: {:#?}", events);
   }
 }
 
@@ -34,8 +34,40 @@ impl VirtualController for Controller {
   }
 }
 
-impl Controller {
-  pub fn run() {
-    println!("Windows is not supported yet. Stay tuned!");
+pub struct WindowsOps;
+
+impl PlatformControllerOps for WindowsOps {
+  type VirtualDevice = ();
+  type PhysicalDevice = ();
+
+  fn create_virtual_controller() -> Result<Self::VirtualDevice, Box<dyn std::error::Error>> {
+    todo!("[Windows] Integrate virtual gamepad using ViGEmBus driver and the vigem-client crate");
+  }
+
+  fn init_mouse() -> Self::PhysicalDevice {
+    todo!("[Windows] Use device_query to detect mouse");
+  }
+
+  fn init_keyboard() -> Self::PhysicalDevice {
+    todo!("[Windows] Use device_query to detect keyboard");
+  }
+
+  fn monitor_io(
+    _mouse: Self::PhysicalDevice,
+    _keyboard: Self::PhysicalDevice,
+    _controller: Arc<Mutex<dyn VirtualControllerCore>>,
+  ) -> ! {
+    todo!("[Windows] Implement event loop for Raw Input (mouse + keyboard)");
+  }
+}
+
+impl PlatformControllerManager for Controller {
+  type Ops = WindowsOps;
+
+  fn try_create() -> Result<Self, Box<dyn std::error::Error>> {
+    Ok(Self {
+      left_stick: Arc::new(Mutex::new(JoyStickState::default())),
+      right_stick: Arc::new(Mutex::new(JoyStickState::default())),
+    })
   }
 }
