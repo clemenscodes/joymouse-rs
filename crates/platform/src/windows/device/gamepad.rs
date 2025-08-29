8a9ceec4aaa0516873368dec3ca432e45da66263
Clemens Horn <clemens.horn@mni.thm.de>
2025-08-29 16:54:00 +0200

    emit some basic button events

--- crates/platform/src/windows/device/gamepad.rs
+++ crates/platform/src/windows/device/gamepad.rs
@@ -1,7 +1,8 @@
-use controller::{ControllerError, ControllerEvent};
-use vigem_client::XGamepad;
-
-use crate::windows::device::buttons::CONTROLLER_BUTTONS;
+use controller::{
+  Axis, ButtonEvent, ControllerButton, ControllerError, ControllerEvent, JoyStick, JoyStickEvent,
+  State,
+};
+use vigem_client::{XButtons, XGamepad};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Gamepad {
@@ -10,10 +11,8 @@ pub struct Gamepad {
 
 impl Default for Gamepad {
   fn default() -> Self {
-    let mut gamepad = XGamepad::default();
-    gamepad.buttons = CONTROLLER_BUTTONS;
     Self {
-      handle: gamepad,
+      handle: XGamepad::default(),
     }
   }
 }
@@ -24,7 +23,98 @@ impl Gamepad {
   }
 
   pub fn update(&mut self, event: &ControllerEvent) -> Result<(), ControllerError> {
-    println!("[Updating gamepad from IO event]: {:#?}", event);
+    use ControllerEvent::*;
+    match event {
+      Button(event) => self.handle_button_event(event),
+      JoyStick(event) => self.handle_joystick_event(event),
+    }
+  }
+
+  fn handle_button_event(&mut self, event: &ButtonEvent) -> Result<(), ControllerError> {
+    use ControllerButton::*;
+    use State::*;
+
+    let button = event.button();
+    let state = event.state();
+
+    if button.eq(&L2) {
+      match state {
+        Pressed | Held => {
+          self.handle.left_trigger = 255;
+        }
+        _ => {
+          self.handle.left_trigger = 0;
+        }
+      };
+      return Ok(());
+    }
+
+    if button.eq(&R2) {
+      match state {
+        Pressed | Held => {
+          self.handle.right_trigger = 255;
+        }
+        _ => {
+          self.handle.right_trigger = 0;
+        }
+      };
+      return Ok(());
+    }
+
+    let button_mask = match button {
+      South => XButtons::A,
+      East => XButtons::B,
+      West => XButtons::X,
+      North => XButtons::Y,
+      Up => XButtons::UP,
+      Down => XButtons::DOWN,
+      Left => XButtons::LEFT,
+      Right => XButtons::RIGHT,
+      L1 => XButtons::LB,
+      R1 => XButtons::RB,
+      L3 => XButtons::LTHUMB,
+      R3 => XButtons::RTHUMB,
+      Start => XButtons::START,
+      Select => XButtons::BACK,
+      Forward | Backward | Starboard | Port => 0,
+      L2 | R2 => 0,
+    };
+
+    match state {
+      Pressed | Held => {
+        self.handle.buttons.raw |= button_mask;
+      }
+      _ => {
+        self.handle.buttons.raw &= !button_mask;
+      }
+    };
+
+    println!("Updated gamepad: {:#?}", self.handle);
+    Ok(())
+  }
+
+  fn handle_joystick_event(&mut self, event: &JoyStickEvent) -> Result<(), ControllerError> {
+    use Axis::*;
+    use JoyStick::*;
+
+    let joystick = event.joystick();
+    let axis = event.axis();
+    let polarity = event.polarity();
+
+    if joystick.eq(&Left) {
+      match axis {
+        X => self.handle.thumb_lx = polarity.into(),
+        Y => self.handle.thumb_ly = polarity.into(),
+      };
+    }
+
+    if joystick.eq(&Left) {
+      match axis {
+        X => self.handle.thumb_rx = polarity.into(),
+        Y => self.handle.thumb_ry = polarity.into(),
+      };
+    }
+
     Ok(())
   }
 }
